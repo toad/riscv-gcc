ec1337166aa2eb50671c981887cd87b84d935d52
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-17 09:58:01 +0100

    Tagged memcpy and memmove
    Similar to those added to the other glibc and newlib.

--- glibc-2.14.1/sysdeps/riscv/memcpy.c
+++ glibc-2.14.1/sysdeps/riscv/memcpy.c
@@ -1,5 +1,8 @@
 #include <string.h>
 #include <stdint.h>
+#include "tag.h"
+
+#define TAGGED_MEMORY
 
 #undef memcpy
 #undef __memcpy_g
@@ -12,13 +15,26 @@ void* __memcpy_g(void* aa, const void* bb, size_t n)
     *(a-1) = tt; \
   }
 
+#ifdef TAGGED_MEMORY
+  #define BODY_TAGGED(a, b) { \
+    long tt = *b; \
+    char tag = load_tag(b); \
+    a++, b++; \
+    *(a-1) = tt; \
+    store_tag(a-1, tag); \
+  }
+#endif
+
   char* a = (char*)aa;
   const char* b = (const char*)bb;
   char* end = a+n;
   uintptr_t msk = sizeof(long)-1;
+#ifdef TAGGED_MEMORY
+  int copyTags = !((uintptr_t)a & msk) && !((uintptr_t)b & msk) && !(n & msk);
+#endif
   if (__builtin_expect(((uintptr_t)a & msk) != ((uintptr_t)b & msk) || n < sizeof(long), 0))
   {
-foo:
+small:
     if (__builtin_expect(a < end, 1))
       while (a < end)
         BODY(a, b, char);
@@ -33,31 +49,75 @@ foo:
   const long* __restrict__ lb = (const long*)b;
   long* lend = (long*)((uintptr_t)end & ~msk);
 
-  if (__builtin_expect(la < lend-8, 0))
-  {
-    while (la < lend-8)
-    {
-      *la++ = *lb++;
-      *la++ = *lb++;
-      *la++ = *lb++;
-      *la++ = *lb++;
-      *la++ = *lb++;
-      *la++ = *lb++;
-      *la++ = *lb++;
-      *la++ = *lb++;
-      *la++ = *lb++;
+#ifdef TAGGED_MEMORY
+  if(copyTags) {
+    if(__builtin_expect(la < lend-3, 0)) {
+      /* Only copy four at a time because will need regs for tags too.
+       * Compiler will use a whole reg for a char, right? */
+      while (la < lend-8)
+      {
+        long b0 = *lb++;
+        char t0 = load_tag(lb-1);
+        long b1 = *lb++;
+        char t1 = load_tag(lb-1);
+        long b2 = *lb++;
+        char t2 = load_tag(lb-1);
+        long b3 = *lb++;
+        char t3 = load_tag(lb-1);
+        *la++ = b0;
+        store_tag(la-1, t0);
+        *la++ = b1;
+        store_tag(la-1, t1);
+        *la++ = b2;
+        store_tag(la-1, t2);
+        *la++ = b3;
+        store_tag(la-1, t3);
+      }
     }
-    if (la == lend)
-      goto bar;
+  } else {
+#endif
+    if(__builtin_expect(la < lend-8, 0)) {
+      while (la < lend-8)
+      {
+        long b0 = *lb++;
+        long b1 = *lb++;
+        long b2 = *lb++;
+        long b3 = *lb++;
+        long b4 = *lb++;
+        long b5 = *lb++;
+        long b6 = *lb++;
+        long b7 = *lb++;
+        long b8 = *lb++;
+        *la++ = b0;
+        *la++ = b1;
+        *la++ = b2;
+        *la++ = b3;
+        *la++ = b4;
+        *la++ = b5;
+        *la++ = b6;
+        *la++ = b7;
+        *la++ = b8;
+      }
+    }
+#ifdef TAGGED_MEMORY
   }
 
-  do BODY(la, lb, long) while (la < lend);
+  if(copyTags) {
+    while (la < lend)
+      BODY_TAGGED(la, lb);
+  } else {
+#endif
+    while (la < lend)
+      BODY(la, lb, long);
+#ifdef TAGGED_MEMORY
+  }
+#endif
 
-bar:
+maybe_done:
   a = (char*)la;
   b = (const char*)lb;
   if (__builtin_expect(a < end, 0))
-    goto foo;
+    goto small;
   return aa;
 }
 libc_hidden_def (__memcpy_g)
---
+++ glibc-2.14.1/sysdeps/riscv/memmove.c
@@ -0,0 +1,86 @@
+/** Memmove for LowRISC (copy tags when all arguments are aligned) */
+
+/* Build the no-tag-copying version of memmove */
+
+#define MEMMOVE __memmove_no_tags
+#include "string/memmove-impl.c"
+#include "tag.h"
+
+/* Nonzero if either X or Y is not aligned on a "long" boundary.  */
+#define UNALIGNED3(X, Y, Z) \
+  (((long)X & (sizeof (long) - 1)) | ((long)Y & (sizeof (long) - 1)) | \
+  ((long)Z & (sizeof (long) - 1)))
+
+
+/* Copy forwards. len in words */
+static inline void wordcopy_fwd_tagged(op_t* dest, op_t* src, size_t len) {
+  /* FIXME consider unrolling, see string/wordcopy.c.
+   * However, the compiler really ought to be able to do that nowadays! */
+  for(;len;dest++,src++,len--) {
+    op_t a = *src;
+    char tag = load_tag(src);
+    *dest = a;
+    store_tag(dest, tag);
+  }
+}
+
+/* Copy backwards. len in words */
+static inline void wordcopy_bwd_tagged(op_t* dest, op_t* src, size_t len) {
+  /* FIXME consider unrolling, see string/wordcopy.c.
+   * However, the compiler really ought to be able to do that nowadays! */
+  dest += (len-1);
+  src += (len-1);
+  for(;len;dest--,src--,len--) {
+    op_t a = *src;
+    char tag = load_tag(src);
+    *dest = a;
+    store_tag(dest, tag);
+  }
+}
+
+/* Tag-copying version of memmove */
+
+void* __memmove_with_tags(op_t* dest, op_t* src, size_t len) {
+  unsigned long int dstp = (long int) dest;
+  unsigned long int srcp = (long int) src;
+  /* This test makes the forward copying code be used whenever possible.
+     Reduces the working set.  */
+  if (dstp - srcp >= len)       /* *Unsigned* compare!  */
+    {
+      /* Copy from the beginning to the end.  */
+
+#if MEMCPY_OK_FOR_FWD_MEMMOVE
+      /* Will include tags */
+      dest = memcpy (dest, src, len);
+#else
+      /* Copy whole pages from SRCP to DSTP by virtual address
+         manipulation, as much as possible.  */
+
+      PAGE_COPY_FWD_MAYBE (dstp, srcp, len, len);
+
+      /* Copy words. We know it is fully aligned so there won't be any
+       * bytes left over. */
+      wordcopy_fwd_tagged(dest, src, len / OPSIZ);
+#endif /* MEMCPY_OK_FOR_FWD_MEMMOVE */
+    } else {
+      /* Copy words backwards. Fully aligned so no bytes left over. */
+      wordcopy_bwd_tagged(dest, src, len / OPSIZ);
+    }
+    return dest;
+}
+
+/* Choose the right version at runtime */
+
+void* __memmove_g (void *dest, const void *src, size_t len) {
+  if(UNALIGNED3(dest, src, len)) {
+    return __memmove_no_tags(dest, src, len);
+  } else {
+    return __memmove_with_tags((op_t*)dest, (op_t*)src, len);
+  }
+  return dest;
+}
+
+libc_hidden_def(__memmove_g)
+strong_alias (__memmove_g, memmove)
+libc_hidden_builtin_def (memmove)
+
